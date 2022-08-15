8bfca78bbc74c76429919d3f81567db25a874307
Bilal <bilal.ibrahim@student.moringaschool.com>
2022-08-15 07:11:08 -0700

    Done with assignnment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+let greeting;
+if (feet <= 400){
+  greeting = 'This one is on me!'
+  return greeting
+}else if (feet >2500){
+  greeting = 'No can do.'
+  return greeting;
+}else if (feet >= 2001){
+  greeting = 'I will gladly take your thirty bucks.'
+  return greeting;
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let answer;
+   if (city === 'NYC'){
+    answer = 'Ok, sounds good.'
+    return answer
+ }else {
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  let gratitude;
+  if (tip === 'generous'){
+    gratitude = 'Thank you so much.'
+    return gratitude;
+  } else if (tip === 'not as generous'){
+    gratitude = 'Thank you.'
+    return gratitude
+  }else {
+    return 'Bye.'
+  }
+
+  }
